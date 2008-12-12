9803697de50af6dcb45caea02ecbefeba0fc566c
Christopher Sexton <csexton@gmail.com>
2008-12-12 18:21:31 -0500

    Added Rspec and some view work.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
- 
+  
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @post }
+    end
+  end
+
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'digest/md5'
+
 module ApplicationHelper
+  # plain old gravatar url  <%= gravatar_url_for 'greenisus@gmail.com' %>    
+  # gravatar url with a rating threshold   <%= gravatar_url_for 'greenisus@gmail.com', { :rating => 'R' } %>    
+  # show the avatar   <%= image_tag(gravatar_url_for 'greenisus@gmail.com')%>   
+  # show the avatar with size specified, in case it's served slowly  <%= image_tag(gravatar_url_for('greenisus@gmail.com'), { :width => 80, :height => 80 }) %>    
+  # link the avatar to some/url  <%= link_to(image_tag(gravatar_url_for 'greenisus@gmail.com'), 'some/url')%> 
+  def gravatar_url_for(email, options = {})
+    url_for({ :gravatar_id => Digest::MD5.hexdigest(email), :host => 'www.gravatar.com',
+            :protocol => 'http://', :only_path => false, :controller => 'avatar.php'
+    }.merge(options))  
+  end
 end
