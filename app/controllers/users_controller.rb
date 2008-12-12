ce64462314631a29b02f6efca52204ca959beb0f
Christopher Sexton <csexton@gmail.com>
2008-12-12 14:10:13 -0500

    Updated controller and helpers from restful-auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   
-
   # render new.rhtml
   def new
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :subscriptions
+
+  map.resources :posts
+
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +43,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "posts"
 
   # See how all your routes lay out with "rake routes"
 
