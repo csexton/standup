6b5b4a5daae7369e250458481044f67a1b5b20e7
Christopher Sexton <csexton@gmail.com>
2008-12-15 13:28:11 -0500

    Added find_by_login to get pretty url's for user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_login(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
